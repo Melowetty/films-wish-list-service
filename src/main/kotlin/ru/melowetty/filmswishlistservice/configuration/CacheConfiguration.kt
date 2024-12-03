7cbfb96e77e3ecc91137b2bd67269f95dcf95b58
Denis Malinin <melowetty@mail.ru>
2024-12-03 18:51:55 +0500

    add caching for external movies

---
+++ src/main/kotlin/ru/melowetty/filmswishlistservice/configuration/CacheConfiguration.kt
@@ -0,0 +1,9 @@
+package ru.melowetty.filmswishlistservice.configuration
+
+import org.springframework.cache.annotation.EnableCaching
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@EnableCaching
+class CacheConfiguration {
+}
--- src/main/kotlin/ru/melowetty/filmswishlistservice/service/impl/CacheableExternalMovieService.kt
+++ src/main/kotlin/ru/melowetty/filmswishlistservice/service/impl/CacheableExternalMovieService.kt
@@ -1,6 +1,7 @@
 package ru.melowetty.filmswishlistservice.service.impl
 
 import mu.KotlinLogging
+import org.springframework.cache.annotation.Cacheable
 import org.springframework.context.annotation.Primary
 import org.springframework.stereotype.Service
 import ru.melowetty.filmswishlistservice.model.ExternalMovie
@@ -14,13 +15,15 @@ class CacheableExternalMovieService(
 ) : ExternalMovieService {
     private val logger = KotlinLogging.logger {  }
 
+    @Cacheable("responses")
     override fun searchMovie(query: String): List<ExternalShortMovie> {
-        logger.info { "Получение данных из кэша" }
+        logger.info { "Получение реальных данных о фильмах из внешнего источника" }
         return externalMovieService.searchMovie(query)
     }
 
+    @Cacheable("movies")
     override fun getMovieByImdbId(imdbId: String): ExternalMovie {
-        logger.info { "Получение данных из кэша" }
+        logger.info { "Получение реальных данных о фильме из внешнего источника" }
         return externalMovieService.getMovieByImdbId(imdbId)
     }
 }
