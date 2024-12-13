898b7d664772c8d7e1879bec8867bde4ce4b63df
Denis Malinin <melowetty@mail.ru>
2024-12-13 17:52:55 +0500

    add validation for user register request

--- src/main/kotlin/ru/melowetty/filmswishlistservice/controller/request/UserRegisterRequest.kt
+++ src/main/kotlin/ru/melowetty/filmswishlistservice/controller/request/UserRegisterRequest.kt
@@ -1,6 +1,14 @@
 package ru.melowetty.filmswishlistservice.controller.request
 
+import jakarta.validation.constraints.NotBlank
+import org.hibernate.validator.constraints.Length
+
 data class UserRegisterRequest(
+    @Length(min = 3, max = 255)
+    @NotBlank
     val username: String,
+    
+    @Length(min = 8)
+    @NotBlank
     val password: String,
 )
